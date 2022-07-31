a5780d56a96e1aabf76713507f327781645ffc24
recepenes <turktenacious@gmail.com>
2022-07-31 20:39:43 +0300

    Add project files Add IRobotPlan

---
+++ src/IRobotPlan.java
@@ -0,0 +1,9 @@
+public interface IRobotPlan {
+    public void setRobotHead(String head);
+
+    public void setRobotTorso(String torso);
+
+    public void setRobotArms(String arms);
+
+    public  void  setRobotLegs(String legs);
+}
